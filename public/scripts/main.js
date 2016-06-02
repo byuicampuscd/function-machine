949804d540a3609931dc7650737391f5cea9ba32
Andrew Gremlich <andrew.gremlich@gmail.com>
2016-06-02 11:09:46 -0600

    Updated according to current wand.js api.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,28 +2,27 @@
     "use strict";
 
     function changePlot(a) {
-       console.log(a);
+        console.log(a);
     }
 
-    var dropdownContain = wand.quer('#dropdown', 'select'),
-       select = wand.crtElm('select'),
-       mathfunc = [
+
+    /****DROPDOWN MAKER****/
+    wand.querApndr('#dropdown', 'select');
+
+    var mathfunc = [
           'x^2',
           'x^3'
        ];
 
-    wand.apndr(dropdownContain, select);
-
     for (var i = 0; i < mathfunc.length; i++) {
-       wand.quer('#dropdown select', 'option');
-       var opt = wand.crtElm('option', mathfunc[i]);
-       wand.apndr(select, opt);
+        wand.querApndr('select', 'option', mathfunc[i]);
     }
 
+    /****DOCUMENT ONCHANGE EVENT HANDLER****/
     document.onchange = function (e) {
-       console.log(e);
-       if (e.target.localName === "select") {
-          changePlot(e.target.value);
-       }
+        console.log(e);
+        if (e.target.localName === "select") {
+            changePlot(e.target.value);
+        }
     }
- }());
+}());
