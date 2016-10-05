256a89cd0c617aab6017a8bcf96f90033203ea52
andrewgremlich <andrew.gremlich@gmail.com>
2016-10-04 23:32:30 -0600

    small cleaning bug fix.

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -2,10 +2,10 @@
 Set up the basic variables.
 */
 var xMemory = [],
-	inputs = document.querySelectorAll("input[type='number']"),
-	inputCount = inputs.length,
-	runMaster = true,
-	run = true;
+    inputs = document.querySelectorAll("input[type='number']"),
+    inputCount = inputs.length,
+    runMaster = true,
+    run = true;
 
 /*
 Attach an oninput event to all the input boxes in order to validate them within the bounds
@@ -14,28 +14,26 @@ and output a message to the status bar.
 */
 for (var i = 0; i < inputs.length; i++) {
 
-	inputs[i].onkeyup = e => {
+    inputs[i].onkeyup = e => {
 
-		var xInputVal = e.srcElement.value;
+        var xInputVal = e.srcElement.value;
 
-		console.log(xInputVal);
-
-		if (e.which === 69) {
-			console.log(xInputVal);
-			e.target.value = "";
-			$("#status p").html(`Can't do that bro!`);
-		} else if (profOpt.view.x.min <= xInputVal && xInputVal <= profOpt.view.x.max) {
-			$("input[type='button'][value='Go!']")
-				.prop("disabled", false)
-				.css({
-					"cursor": "pointer"
-				});
-			$("#status p").html(``);
-		} else {
-			e.target.value = "";
-			$("#status p").html(`${xInputVal} x-value out of domains.`);
-		}
-	};
+        if (e.which === 69) {
+            console.log(xInputVal);
+            e.target.value = "";
+            $("#status p").html(`Can't do that bro!`);
+        } else if (profOpt.view.x.min <= xInputVal && xInputVal <= profOpt.view.x.max) {
+            $("input[type='button'][value='Go!']")
+                .prop("disabled", false)
+                .css({
+                    "cursor": "pointer"
+                });
+            $("#status p").html(``);
+        } else {
+            e.target.value = "";
+            $("#status p").html(`${xInputVal} x-value out of domains.`);
+        }
+    };
 }
 
 /*
@@ -43,30 +41,30 @@ Dipslay Katex equation. ALSO used in ajax.js
 */
 function changePlot(val) {
 
-	xMemory = [];
+    xMemory = [];
 
-	clearValues();
+    clearValues();
 
-	window.profOpt = checkConfig(val);
-	var y = `y = `,
-		equat = `${val}`,
-		equPara = $("#functionMachine #equ")[0],
-		yPara = $("#functionMachine #y");
+    window.profOpt = checkConfig(val);
+    var y = `y = `,
+        equat = `${val}`,
+        equPara = $("#functionMachine #equ")[0],
+        yPara = $("#functionMachine #y");
 
-	$(equPara).empty("");
+    $(equPara).empty("");
 
-	katex.render(y, yPara[0]);
-	katex.render(equat, equPara);
+    katex.render(y, yPara[0]);
+    katex.render(equat, equPara);
 }
 
 /*
 Animation path for the stairstep
 */
 function stairStep(options) {
-	"use strict";
-	var highwayPath = 246.5,
-		lastSheet = document.styleSheets[document.styleSheets.length - 1];
-	lastSheet.insertRule(`@keyframes ${options.name} {
+    "use strict";
+    var highwayPath = 246.5,
+        lastSheet = document.styleSheets[document.styleSheets.length - 1];
+    lastSheet.insertRule(`@keyframes ${options.name} {
                             0% {
                                 opacity: 1;
                                 top: ${options.startTopOff}px;
@@ -99,36 +97,36 @@ A set of functions using the stairstep animation template
 to create pathways with coordinate data
 */
 function makeXToMachine(inputCords, index) {
-	"use strict";
-	stairStep({
-		startTopOff: inputCords.top + 5,
-		startLeftOff: inputCords.left + 30,
-		endTopOff: 55,
-		endLeftOff: 300,
-		name: `xToMachine${index}`
-	});
+    "use strict";
+    stairStep({
+        startTopOff: inputCords.top + 5,
+        startLeftOff: inputCords.left + 30,
+        endTopOff: 55,
+        endLeftOff: 300,
+        name: `xToMachine${index}`
+    });
 }
 
 function makeMachineToY(inputCords, index) {
-	"use strict";
-	stairStep({
-		startTopOff: 100,
-		startLeftOff: 530,
-		endTopOff: inputCords.top + 5,
-		endLeftOff: inputCords.right + 5,
-		name: `machineToY${index}`
-	});
+    "use strict";
+    stairStep({
+        startTopOff: 100,
+        startLeftOff: 530,
+        endTopOff: inputCords.top + 5,
+        endLeftOff: inputCords.right + 5,
+        name: `machineToY${index}`
+    });
 }
 
 function makeYToStatusBar(inputCords, index) {
-	"use strict";
-	stairStep({
-		startTopOff: inputCords.top + 5,
-		startLeftOff: inputCords.right + 10,
-		endTopOff: 150,
-		endLeftOff: 300,
-		name: `yToStatusBar${index}`
-	});
+    "use strict";
+    stairStep({
+        startTopOff: inputCords.top + 5,
+        startLeftOff: inputCords.right + 10,
+        endTopOff: 150,
+        endLeftOff: 300,
+        name: `yToStatusBar${index}`
+    });
 }
 
 /*
@@ -138,14 +136,14 @@ NOTE: Setting up the xMemory array also makes it so that no animations
       are repeated by multiple clicks on the "Go!" button.
 */
 for (var i = 0; i < inputCount; i++) {
-	xMemory[i] = null;
+    xMemory[i] = null;
 
-	var inputCoor = inputs[i].getBoundingClientRect();
+    var inputCoor = inputs[i].getBoundingClientRect();
 
-	makeXToMachine(inputCoor, i);
-	makeMachineToY(inputCoor, i);
-	makeYToStatusBar(inputCoor, i);
-	$("#numContainer").append($(`<p></p>`));
+    makeXToMachine(inputCoor, i);
+    makeMachineToY(inputCoor, i);
+    makeYToStatusBar(inputCoor, i);
+    $("#numContainer").append($(`<p></p>`));
 }
 
 /*
@@ -153,45 +151,45 @@ Set up the object that will be passed through the promise chain
 in animatorcontrol.
 */
 function setUpObject(xinputs, graphOpt, aniSettings) {
-	xinputs.each(function (i) {
+    xinputs.each(function (i) {
 
-		var xvalue = $(this).val(),
-			xval,
-			roundit;
+        var xvalue = $(this).val(),
+            xval,
+            roundit;
 
-		if (xvalue) {
-			xval = +xvalue;
-			roundit = xval.toFixed(profOpt.rounding);
+        if (xvalue) {
+            xval = +xvalue;
+            roundit = xval.toFixed(profOpt.rounding);
 
-			$(this).val(roundit);
+            $(this).val(roundit);
 
-			if (profOpt.view.x.min <= roundit && roundit <= profOpt.view.x.max) {
+            if (profOpt.view.x.min <= roundit && roundit <= profOpt.view.x.max) {
 
-				var replaceX = graphOpt.equation.replace("x", `(${roundit})`),
-					yval = math.eval(replaceX),
-					inputCoor = this.getBoundingClientRect(),
-					point = {
-						x: roundit,
-						y: yval.toFixed(profOpt.rounding),
-						id: i,
-						changeEqu: profOpt.equation.replace("x", `(${roundit})`),
-						updatePoint: xMemory[i] !== roundit,
-						element: $("#numContainer p").get(i)
-					};
+                var replaceX = graphOpt.equation.replace("x", `(${roundit})`),
+                    yval = math.eval(replaceX),
+                    inputCoor = this.getBoundingClientRect(),
+                    point = {
+                        x: roundit,
+                        y: yval.toFixed(profOpt.rounding),
+                        id: i,
+                        changeEqu: profOpt.equation.replace("x", `(${roundit})`),
+                        updatePoint: xMemory[i] !== roundit,
+                        element: $("#numContainer p").get(i)
+                    };
 
-				/*
-				Clear out the Ys when they don't equal each other and need to be updated
-				*/
-				if (point.updatePoint) {
-					$(`td#yval${i + 1}`).html("");
-				}
+                /*
+                Clear out the Ys when they don't equal each other and need to be updated
+                */
+                if (point.updatePoint) {
+                    $(`td#yval${i + 1}`).html("");
+                }
 
-				/*Update the xmemory*/
-				xMemory[i] = roundit;
-				aniSettings.datapoints.push(point);
-			}
-		}
-	});
+                /*Update the xmemory*/
+                xMemory[i] = roundit;
+                aniSettings.datapoints.push(point);
+            }
+        }
+    });
 }
 
 /*
@@ -202,124 +200,125 @@ graphOpt.callback still needs a viable method!
 */
 function startFuncMach() {
 
-	var xinputs = $("input[type='number']"),
-		hideAnimationChecked = $("#animate:checked").length > 0,
-		hideGraphChecked = $("#showGraph:checked").length > 0,
-		graphOpt = {
-			callback: function (aniSettings) {
-				return new Promise(function (resolve) {
-					aniSettings.datapoints[aniSettings.currentRound].updatePoint = false;
-					resolve(aniSettings);
-				});
-			},
-			animateHide: hideAnimationChecked,
-			graphHide: hideGraphChecked,
-			equation: profOpt.equation,
-			view: profOpt.view
-		},
-		aniSettings = {
-			datapoints: [],
-			currentRound: 0,
-			graphOpt: graphOpt
-		};
+    var xinputs = $("input[type='number']"),
+        hideAnimationChecked = $("#animate:checked").length > 0,
+        hideGraphChecked = $("#showGraph:checked").length > 0,
+        graphOpt = {
+            callback: function (aniSettings) {
+                return new Promise(function (resolve) {
+                    aniSettings.datapoints[aniSettings.currentRound].updatePoint = false;
+                    resolve(aniSettings);
+                });
+            },
+            animateHide: hideAnimationChecked,
+            graphHide: hideGraphChecked,
+            equation: profOpt.equation,
+            view: profOpt.view
+        },
+        aniSettings = {
+            datapoints: [],
+            currentRound: 0,
+            graphOpt: graphOpt
+        };
 
-	setUpObject(xinputs, graphOpt, aniSettings);
+    setUpObject(xinputs, graphOpt, aniSettings);
 
-	animatorControl(aniSettings);
+    animatorControl(aniSettings);
 }
 
 /*
 Function to select the chosen equation with its name and graph window boundaries.
 */
 function checkConfig(val) {
-	var profOpt;
-	$.each(professorConfigFile, function (i, item) {
-		if (item.equation === val) {
-			profOpt = item;
-		}
-	})
-	return profOpt;
+    var profOpt;
+    $.each(professorConfigFile, function (i, item) {
+        if (item.equation === val) {
+            profOpt = item;
+        }
+    })
+    return profOpt;
 }
 
 /*
 Upon choosing another equation to graph, clear all the values
 */
 function clearValues() {
-	var xinputs = $("input[type='number']"),
-		yinputs = $(`tr td:nth-of-type(2)`);
+    var xinputs = $("input[type='number']"),
+        yinputs = $(`tr td:nth-of-type(2)`);
 
-	xinputs.each(function (i, item) {
-		item.value = "";
-	});
+    xinputs.each(function (i, item) {
+        item.value = "";
+    });
 
-	yinputs.each(function (i, item) {
-		item.innerHTML = "";
-	});
+    yinputs.each(function (i, item) {
+        item.innerHTML = "";
+    });
 }
 
 /*Before running the function machine, put all inputs next to each other.*/
 function cleanInputs() {
-	var xinputs = $("input[type='number']"),
-		inputArray = [];
+    var xinputs = $("input[type='number']"),
+        inputArray = [];
 
-	xinputs.each(function (i) {
-		var inputValue = $(this).val();
+    xinputs.each(function (i) {
+        var inputValue = $(this).val();
 
-		inputArray.push(inputValue);
-		$(this).val("");
+        if (inputValue !== "") {
+            inputArray.push(inputValue);
+            $(this).val("");
+        }
+    })
 
-	})
-
-	xinputs.each(function (i) {
-		if (i < inputArray.length) {
-			$(this).val(inputArray[i]);
-		} else {
-			return false;
-		}
-	})
+    xinputs.each(function (i) {
+        if (i < inputArray.length) {
+            $(this).val(inputArray[i]);
+        } else {
+            $(this).val("");
+        }
+    })
 }
 
 /*
 Onchange event handler for the select html element.
 */
 $("select").change(function (e) {
-	var selected = $(`option[value="${e.target.value}"]`),
-		profOpt = JSON.parse(selected.attr("data-profopt"));
+    var selected = $(`option[value="${e.target.value}"]`),
+        profOpt = JSON.parse(selected.attr("data-profopt"));
 
-	plotGraph.setup(profOpt, "#graph")
-	changePlot(e.target.value);
+    plotGraph.setup(profOpt, "#graph")
+    changePlot(e.target.value);
 });
 
 /*Checkbox onclick event*/
 if ($("input#showGraph[type='checkbox']").attr("checked") === "checked") {
-	$("#graph").hide();
+    $("#graph").hide();
 }
 
 $("input#showGraph[type='checkbox']").click(e => {
-	var checked = e.target.checked
-	if (checked) {
-		$("#graph").hide(1000);
-	} else {
-		$("#graph").show(1000);
-	}
+    var checked = e.target.checked
+    if (checked) {
+        $("#graph").hide(1000);
+    } else {
+        $("#graph").show(1000);
+    }
 })
 
 /*
 DOCUMENT keydown event handler
 */
 $(document).keypress(function (e) {
-	if (e.which == 13 && runMaster) {
-		cleanInputs();
-		startFuncMach();
-	}
+    if (e.which == 13 && runMaster) {
+        cleanInputs();
+        startFuncMach();
+    }
 });
 
 /*
 GO! Click event handler
 */
 $("input[type='button'][value='Go!']").click(function () {
-	if (runMaster) {
-		cleanInputs();
-		startFuncMach();
-	}
+    if (runMaster) {
+        cleanInputs();
+        startFuncMach();
+    }
 });
