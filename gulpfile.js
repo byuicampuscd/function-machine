c3de515434b25c4631dc587fa57308448261c72d
Didymus <didymus.benson@gmail.com>
2018-02-14 11:54:34 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,10 @@ gulp.task('scripts', function () {
          this.emit('end');
       }))
       .pipe(sourcemaps.init())
-//      .pipe(babel({
-//         presets: ['es2015']
-//      }))
-//      .pipe(uglify())
+      .pipe(babel({
+         presets: ['es2015']
+      }))
+      .pipe(uglify())
       .pipe(concat('scripts.js'))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(DIST_PATH))
